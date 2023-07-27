2a7d8549e3548fc0481c84fc989c55b3537aa1bc
Muhammed Atmaca <rmk.atmaca@gmail.com>
2023-07-27 15:26:23 +0300

    Money system polish done

--- DeepDiver/Assets/!Scripts/System Managers/MoneyManager.cs
+++ DeepDiver/Assets/!Scripts/System Managers/MoneyManager.cs
@@ -3,8 +3,25 @@ using UnityEngine;
 
 public class MoneyManager : MonoSingleton<MoneyManager>
 {
+    [Header("Money UI Element")]
     [SerializeField] private TextMeshProUGUI _moneyTMP;
+    [Header("Current Player Money")]
     [SerializeField] private int _currentPlayerMoney;
+
+    [Header("Money Change Lerp Time")]
+    [SerializeField]private float _lerpTime;
+
+    private int _startMoney;
+    private int _endMoney;
+    private float _timeStartedLerping;
+
+    private bool timerRunning = false;
+    private float _timer;
+
+    private void Awake()
+    {
+        _timer = _lerpTime;
+    }
 
     private void Start()
     {
@@ -12,17 +29,61 @@ public class MoneyManager : MonoSingleton<MoneyManager>
         _moneyTMP.text = _currentPlayerMoney.ToString();
     }
 
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GainMoney(100);
+        }
+        else if (Input.GetKeyDown(KeyCode.D))
+        {
+            SpendMoney(100);
+        }
+
+        if (!timerRunning) return;
+
+        _timer -= Time.smoothDeltaTime;
+        if (_timer >= 0)
+        {
+            _moneyTMP.text = LerpInteger(_startMoney, _endMoney, _timeStartedLerping, _lerpTime).ToString();
+        }
+        else
+        {
+            timerRunning = false;
+            _moneyTMP.text = _currentPlayerMoney.ToString();
+            _timer = _lerpTime;
+        }
+    }
+
     public void GainMoney(int gainAmount)
     {
+        StartLerping(+gainAmount);
         _currentPlayerMoney += gainAmount;
         SaveManager.Instance.SavePlayerMoney(_currentPlayerMoney);
-        _moneyTMP.text = _currentPlayerMoney.ToString();
     }
 
     public void SpendMoney(int spendAmount)
     {
+        StartLerping(-spendAmount);
         _currentPlayerMoney -= spendAmount;
         SaveManager.Instance.SavePlayerMoney(_currentPlayerMoney);
-        _moneyTMP.text = _currentPlayerMoney.ToString();
+    }
+
+    private void StartLerping(int changeAmount)
+    {
+        _timeStartedLerping = Time.time;
+        _startMoney = _currentPlayerMoney;
+        _endMoney = _currentPlayerMoney + changeAmount;
+        timerRunning = true;
+    }
+
+    private int LerpInteger(int start, int end, float timeStarted, float lerpTime = 1)
+    {
+        float timeSinceStarted = Time.time - timeStarted;
+        float percentageComplete = timeSinceStarted / lerpTime;
+
+        int result = (int)Mathf.Lerp(start, end, percentageComplete);
+
+        return result;
     }
 }
