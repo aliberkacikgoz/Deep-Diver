762438ec6c1beb38267bae2786187d035b068567
Omer Faruk Kilic <omeerfkiliic@gmail.com>
2023-07-30 22:17:47 +0300

    Camera done

--- DeepDiver/Assets/CameraStateHandler.cs
+++ DeepDiver/Assets/CameraStateHandler.cs
@@ -1,18 +1,59 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraStateHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] TriggerChannelSO _swimmingStartTrigger;
+    [SerializeField] TriggerChannelSO _walkingStartTrigger;
+    [SerializeField] TriggerChannelSO _submarineStartTrigger;
+
+    [SerializeField] GameObject _inSubCam;
+    [SerializeField] GameObject _subCam;
+    [SerializeField] GameObject _swimCam;
+
+    private void OnEnable()
     {
-        
+        _swimmingStartTrigger.AddListener(ActivateSwimmingCam);
+        _walkingStartTrigger.AddListener(ActivateWalkingCam);
+        _submarineStartTrigger.AddListener(ActivateSubmarineCam);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        _swimmingStartTrigger.RemoveListener(ActivateSwimmingCam);
+        _walkingStartTrigger.RemoveListener(ActivateWalkingCam);
+        _submarineStartTrigger.RemoveListener(ActivateSubmarineCam);
+    }
+
+    private void Start()
+    {
+        ActivateSubmarineCam();
+    }
+
+    private void ActivateSubmarineCam()
+    {
+        _inSubCam.SetActive(false);
+        _swimCam.SetActive(false);
+
+        _subCam.SetActive(true);
+    }
+
+    private void ActivateWalkingCam()
+    {
+        _swimCam.SetActive(false);
+        _subCam.SetActive(false);
+
+        _inSubCam.SetActive(true);
+    }
+
+    private void ActivateSwimmingCam()
+    {
+        _inSubCam.SetActive(false);
+        _subCam.SetActive(false);
+
+        _swimCam.SetActive(true);
+
     }
 }
